9d6565b0659efe9b7be1e7f1ef6a814be732870f
Vitor Oliveira <vmoliveira91@gmail.com>
2020-08-04 22:50:22 -0300

    Aula 02

---
+++ aulas/server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Corpo (request body): dados p/criação ou atualização request.body
+// Route Params: identificar qual recurso quero atualizar ou deletar request.params
+// :id
+// Query Params: paginação, filtro, ordenação request.query
+// ?page=2&sort=name
