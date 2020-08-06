c445b0dd25a616eff9509ada420363040d2b2eda
Vitor Oliveira <vmoliveira91@gmail.com>
2020-08-06 00:05:08 -0300

    Alteração da porta de escuta

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors);
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3000);
 
 // Corpo (request body): dados p/criação ou atualização request.body
 // Route Params: identificar qual recurso quero atualizar ou deletar request.params
